29718cb07e3e0a74b692367b77520afe6cb61c83
xitsssr <theMinimalistFinance@gmail.com>
2025-10-21 23:51:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Automatically highlight dots on i and j
+//document.addEventListener('DOMContentLoaded', () => {
+//  const quoteElement = document.getElementById('quote');
+//  const text = quoteElement.innerText;
+//  const transformed = text.replace(/([ij])/g, '<span class="dot">$1</span>');
+//  quoteElement.innerHTML = transformed;
+//});
